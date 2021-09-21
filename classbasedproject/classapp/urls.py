9009678ff1412a084e7ceff1d36a11aec0b9e054
abdullakn <abdudebanjazz@gmail.com>
2021-09-21 11:25:23 +0530

    Redirect view completed

--- classbasedproject/classapp/urls.py
+++ classbasedproject/classapp/urls.py
@@ -1,10 +1,18 @@
 
+from django.conf.urls import url
 from django.urls import path
 from django.views.generic import TemplateView
-from . views import Sample1
+from . views import Sample1,Sample2,Sample3
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
    
     path('',TemplateView.as_view(template_name='index.html' , extra_context={'title':"my title"})),
-    path('sample1/',Sample1.as_view())
+    path('sample1/',Sample1.as_view()),
+
+
+    #redirect view
+    path('abdullakn.online',RedirectView.as_view(url='https://abdullakn.online')),
+    path('sample2/<int:pk>/',Sample2.as_view(),name='sample2'),
+    path('sample3/<int:pk>/',Sample3.as_view(),name='sample3')
 ]
--- classbasedproject/classapp/views.py
+++ classbasedproject/classapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView,RedirectView
 from . models import *
+from django.shortcuts import get_object_or_404
+
 
 
 # Create your views here.
@@ -15,4 +17,30 @@ class Sample1(TemplateView):
         print(context['posts'])
         return context
 
-       
+
+class Sample2(RedirectView):
+    # url='https://abdullakn.online'
+    pattern_name = 'sample3'
+    permanent=True
+
+             
+
+    def get_redirect_url(self, *args, **kwargs):
+        # article = get_object_or_404(Post, pk=kwargs['pk'])
+        # print(article)
+        print(args)
+        print(kwargs)
+        print(super().get_redirect_url(*args, **kwargs))
+        
+        return super().get_redirect_url(*args, **kwargs)
+
+
+class Sample3(TemplateView):
+    template_name='detailsredirect.html'
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['posts']=get_object_or_404(Post,pk=self.kwargs.get('pk'))
+        return context        
+
+        
